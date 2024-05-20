021fd26d3da4cc23259a1cd289c13bc70e447634
MaKarastY <71918286+makarasty@users.noreply.github.com>
2024-05-20 18:26:07 +0300

    total fix

--- ModulesTest/UnitTest1.cs
+++ ModulesTest/UnitTest1.cs
@@ -22,12 +22,14 @@ public class SumModuleTests
 	}
 
 	[Theory]
-	[InlineData("abc", "20")]
-	[InlineData("", "123")]
-	public void Sum_InvalidInputFormat_ThrowsArgumentExceptionForParam1(string param1, string param2)
+	[InlineData("abc", "123", "param1")]
+	[InlineData("", "123", "param1")]
+	[InlineData("123", "abc", "param2")]
+	[InlineData("123", "", "param2")]
+	public void Sum_InvalidInputFormat_ThrowsArgumentExceptionForParam1(string param1, string param2, string invalidParamName)
 	{
 		// Arrange
-		var param1ErrorText = "Не вірний формат числа для параметра param1";
+		var param1ErrorText = $"Не вірний формат числа для параметра {invalidParamName}";
 
 		// Act
 		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
@@ -35,77 +37,58 @@ public class SumModuleTests
 		// Assert
 		Assert.Contains(param1ErrorText, ex.Message);
 	}
+
 	[Theory]
-	[InlineData("10", "def")]
-	[InlineData("456", "")]
-	public void Sum_InvalidInputFormat_ThrowsArgumentExceptionForParam2(string param1, string param2)
+	[InlineData("param1")]
+	[InlineData("param2")]
+	public void OutOfMaxRange_ThrowsArgumentException_WithCurrentText(string paramName)
 	{
 		// Arrange
-		var param2ErrorText = "Не вірний формат числа для параметра param2";
+		var paramErrorText = $"Перевищено максимальне значення для параметра {paramName}";
 
-		// Act
-		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
-
-		// Assert
-		Assert.Contains(param2ErrorText, ex.Message);
-	}
-
-	[Fact]
-	public void Param1_OutOfMaxRange_ThrowsArgumentException_WithCurrentText()
-	{
-		// Arrange
-		var param1ErrorText = "Перевищено максимальне значення для параметра param1";
-		var param1 = ((long)int.MaxValue + 1).ToString();
+		var param1 = "0";
 		var param2 = "0";
 
+		if (paramName == "param1")
+		{
+			param1 = ((long)int.MaxValue + 1).ToString();
+		}
+		else
+		{
+			param2 = ((long)int.MaxValue + 1).ToString();
+		}
+
 		// Act
 		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
 
 		// Assert
-		Assert.Contains(param1ErrorText, ex.Message);
+		Assert.Contains(paramErrorText, ex.Message);
 	}
 
-	[Fact]
-	public void Param2_OutOfMaxRange_ThrowsArgumentException_WithCurrentText()
+	[Theory]
+	[InlineData("param1")]
+	[InlineData("param2")]
+	public void OutOfMinRange_ThrowsArgumentException_WithCurrentText(string paramName)
 	{
 		// Arrange
+		var paramErrorText = $"Перевищено мінімальне значення для параметра {paramName}";
+
 		var param1 = "0";
-		var param2ErrorText = "Перевищено максимальне значення для параметра param2";
-		var param2 = ((long)int.MaxValue + 1).ToString();
-
-		// Act
-		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
-
-		// Assert
-		Assert.Contains(param2ErrorText, ex.Message);
-	}
-
-	[Fact]
-	public void Param1_OutOfMinRange_ThrowsArgumentException_WithCurrentText()
-	{
-		// Arrange
-		var param1ErrorText = "Перевищено мінімальне значення для параметра param1";
-		var param1 = ((long)int.MinValue - 1).ToString();
 		var param2 = "0";
 
-		// Act
-		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
-
-		// Assert
-		Assert.Contains(param1ErrorText, ex.Message);
-	}
-	[Fact]
-	public void Param2_OutOfMinRange_ThrowsArgumentException_WithCurrentText()
-	{
-		// Arrange
-		var param1 = "0";
-		var param2ErrorText = "Перевищено мінімальне значення для параметра param2";
-		var param2 = ((long)int.MinValue - 1).ToString();
+		if (paramName == "param1")
+		{
+			param1 = ((long)int.MinValue - 1).ToString();
+		}
+		else
+		{
+			param2 = ((long)int.MinValue - 1).ToString();
+		}
 
 		// Act
 		var ex = Assert.Throws<ArgumentException>(() => SumModule.Sum(param1, param2));
 
 		// Assert
-		Assert.Contains(param2ErrorText, ex.Message);
+		Assert.Contains(paramErrorText, ex.Message);
 	}
 }
